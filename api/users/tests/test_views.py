c9536f79cdadfe63fe876e1c32ff9d3074ca5291
tanvir <tanvir.hasan@codemarshal.com>
2020-10-22 13:38:54 +0600

    feat(api): add test coverage for registration endpoint

--- api/users/tests/test_views.py
+++ api/users/tests/test_views.py
@@ -25,3 +25,48 @@ class TestUserMeEndPoint(APITestCase):
         self.client.force_authenticate(user=None)
         response = self.client.get(self.me_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class TestUserRegistrationEndpoint(APITestCase):
+    registration_url = reverse('users:users-list')
+
+    def setUp(self):
+        self.user = User.objects.create_user(email='test@email.com',
+                                             password="password")
+
+    def test_registration_success_with_valid_data(self):
+        data = {
+            'email': 'test2@email.com',
+            'password': 'password-super',
+            're_password': 'password-super'
+        }
+        response = self.client.post(self.registration_url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_registration_failed_without_re_password(self):
+        data = {
+            'email': 'test2@email.com',
+            'password': 'password-super',
+        }
+        response = self.client.post(self.registration_url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_registration_failed_with_worng_re_password(self):
+        data = {
+            'email': 'test2@email.com',
+            'password': 'password-super',
+            're_password': 'password'
+        }
+        response = self.client.post(self.registration_url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_registration_failed_with_existing_email(self):
+        data = {
+            'email': 'test@email.com',
+            'password': 'password-super',
+            're_password': 'password-super'
+        }
+        response = self.client.post(self.registration_url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['email'][0],
+                         'user with this email address already exists.')
